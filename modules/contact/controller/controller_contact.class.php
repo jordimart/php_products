4756c104b9b38d018a25f64645b0fa886beddc68
Jorge <jordimart83@gmail.com>
2016-11-26 10:47:08 +0100

    He añadido el codigo del módulo de contact para refactorizar

--- modules/contact/controller/controller_contact.class.php
+++ modules/contact/controller/controller_contact.class.php
@@ -1,54 +1,75 @@
+<?php
 
-<!--=========== BEGIN CONTACT SECTION ================-->
-    <section id="contact">
-      <div class="container">
-       <div class="row">
-          <div class="col-lg-12 col-md-12"> 
-            <div class="title_area">
-              <h2 class="title_two">We are Wpf Degree</h2>
-              <span></span> 
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
-            </div>
-          </div>
-       </div>
-       <div class="row">
-         <div class="col-lg-8 col-md-8 col-sm-8">
-           <div class="contact_form wow fadeInLeft">
-              <form class="submitphoto_form">
-                <input type="text" class="wp-form-control wpcf7-text" placeholder="Your name">
-                <input type="mail" class="wp-form-control wpcf7-email" placeholder="Email address">          
-                <input type="text" class="wp-form-control wpcf7-text" placeholder="Subject">
-                <textarea class="wp-form-control wpcf7-textarea" cols="30" rows="10" placeholder="What would you like to tell us"></textarea>
-                <input type="submit" value="Submit" class="wpcf7-submit">
-              </form>
-           </div>
-         </div>
-         <div class="col-lg-4 col-md-4 col-sm-4">
-           <div class="contact_address wow fadeInRight">
-             <h3>Address</h3>
-             <div class="address_group">
-               <p>200 Lincoln Ave, Salinas, CA 93901</p>
-               <p>Phone: 662-807-9585</p>
-               <p>Email:contact@wpfdegree.com</p>
-             </div>
-             <div class="address_group">
-              <ul class="footer_social">
-                <li><a href="#" class="soc_tooltip" title="" data-placement="top" data-toggle="tooltip" data-original-title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#" class="soc_tooltip" title="" data-placement="top" data-toggle="tooltip" data-original-title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#" class="soc_tooltip" title="" data-placement="top" data-toggle="tooltip" data-original-title="Google+"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#" class="soc_tooltip" title="" data-placement="top" data-toggle="tooltip" data-original-title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                <li><a href="#" class="soc_tooltip" title="" data-placement="top" data-toggle="tooltip" data-original-title="Youtube"><i class="fa fa-youtube"></i></a></li>
-                </ul>
-             </div>
-           </div>
-         </div>
-       </div>
-      </div>
-    </section>
-    <!--=========== END CONTACT SECTION ================-->
+class controller_contact {
 
-    <!--=========== BEGIN GOOGLE MAP SECTION ================-->
-    <section id="googleMap">
-      <iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=200+Lincoln+Ave,+Salinas,+CA,+USA&amp;aq=&amp;sll=30.977609,-95.712891&amp;sspn=42.157377,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=200+Lincoln+Ave,+Salinas,+California+93901-2639&amp;t=m&amp;z=14&amp;ll=36.674837,-121.657691&amp;output=embed"></iframe>
-    </section>
-    <!--=========== END GOOGLE MAP SECTION ================-->
+    public function __construct() {
+        $_SESSION['module'] = "contact";
+    }
+
+    public function view_contact() {
+        require_once(VIEW_PATH_INC . "header.html");
+        require_once(VIEW_PATH_INC . "menu.php");
+
+        loadView(CONTACT_VIEW_PATH, 'contact.php');
+
+        require_once(VIEW_PATH_INC . "footer.html");
+    }
+
+    public function process_contact() {
+        if ($_POST['token'] === "contact_form") {
+
+            //////////////// Envio del correo al usuario
+            $arrArgument = array(
+                'type' => 'contact',
+                'token' => '',
+                'inputName' => $_POST['inputName'],
+                'inputEmail' => $_POST['inputEmail'],
+                'inputSubject' => $_POST['inputSubject'],
+                'inputMessage' => $_POST['inputMessage']
+            );
+            set_error_handler('ErrorHandler');
+            try {
+                /*
+                  if (enviar_email($arrArgument)){
+                  echo "<div class='alert alert-success'>Your message has been sent </div>";
+                  } else {
+                  echo "<div class='alert alert-error'>Server error. Try later...</div>";
+                  }
+                 */
+                echo "<div class='alert alert-success'>" . enviar_email($arrArgument) . " </div>";
+            } catch (Exception $e) {
+                echo "<div class='alert alert-error'>Server error. Try later...</div>";
+            }
+            restore_error_handler();
+
+
+            //////////////// Envio del correo al admin de la ap web
+            $arrArgument = array(
+                'type' => 'admin',
+                'token' => '',
+                'inputName' => $_POST['inputName'],
+                'inputEmail' => $_POST['inputEmail'],
+                'inputSubject' => $_POST['inputSubject'],
+                'inputMessage' => $_POST['inputMessage']
+            );
+            set_error_handler('ErrorHandler');
+            try {
+                /*
+                  if (enviar_email($arrArgument)){
+                  echo "<div class='alert alert-success'>Your message has been sent </div>";
+                  } else {
+                  echo "<div class='alert alert-error'>Server error. Try later...</div>";
+                  }
+                 */
+                sleep(5);
+                echo "<div class='alert alert-success'>" . enviar_email($arrArgument) . " </div>";
+            } catch (Exception $e) {
+                echo "<div class='alert alert-error'>Server error. Try later...</div>";
+            }
+            restore_error_handler();
+        } else {
+            echo "<div class='alert alert-error'>Server error. Try later...</div>";
+        }
+    }
+
+}
